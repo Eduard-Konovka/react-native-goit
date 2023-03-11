41b2f59fa525a81e67208d96208b180586eb872c
Eduard Konovka <ed098ua@gmail.com>
2023-03-11 22:30:55 +0200

    Adds working with the keyboard

--- App.js
+++ App.js
@@ -1,30 +1,55 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Switch, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 export default function App() {
   const [isEnabled, setIsEnabled] = useState(false);
   const [swichResult, setSwichResult] = useState('Disabled switch');
+  const [value, setValue] = useState('');
 
   const toggleSwitch = () => {
     setIsEnabled(previousState => !previousState);
     setSwichResult(isEnabled ? 'Disabled switch' : 'Enabled switch');
   };
 
+  const inputHandler = text => setValue(text);
+
   return (
-    <View style={styles.container}>
-      <Text>{swichResult}</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text>{swichResult}</Text>
 
-      <Switch
-        trackColor={{ false: '#767577', true: '#81b0ff' }}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+        <Switch
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
 
-      <StatusBar style="auto" />
-    </View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        >
+          <TextInput
+            placeholder="Type text"
+            value={value}
+            onChangeText={inputHandler}
+          />
+        </KeyboardAvoidingView>
+
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
